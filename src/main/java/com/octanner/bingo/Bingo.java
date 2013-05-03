99e397b89fea7bf28cfca1923080ea4158a400bd
Ethan Kennelly <ethan.kennelly@i26811.octanner.com>
2013-05-03 09:57:52 -0600

    initial commit

---
+++ src/main/java/com/octanner/bingo/Bingo.java
@@ -0,0 +1,11 @@
+package com.octanner.bingo;
+
+import org.apache.log4j.Logger;
+
+
+public class Bingo
+{
+    private static final Logger log = Logger.getLogger(Bingo.class);
+
+
+}
---
+++ src/test/java/com/octanner/bingo/BingoTest.java
@@ -0,0 +1,35 @@
+package com.octanner.bingo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.*;
+
+public class BingoTest
+{
+
+
+
+    @Before
+    public void before()
+    {
+    }
+
+    @After
+    public void after()
+    {
+    }
+
+    @Test
+    public void testNewBingoGame() throws Exception
+    {
+        assertTrue(false);
+    }
+
+
+
+}
